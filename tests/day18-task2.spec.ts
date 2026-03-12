925b94a3190543c8a3eb98aae07bd7a187746a7b
Hitanshi <hitanshi.1003@gmail.com>
2026-03-12 22:51:32 +0530

    day 18

---
+++ tests/day18-task2.spec.ts
@@ -0,0 +1,18 @@
+import {test,expect,chromium} from "@playwright/test";
+
+test("Browser Notification",async()=>{
+ const browser = await chromium.launch({ headless: false });
+  let context=await browser.newContext({
+    permissions:["notifications"]
+  });
+  let page=await context.newPage();
+  await page.goto("https://www.justdial.com");
+  let result=await page.evaluate(async()=>{
+    return await Notification.requestPermission();
+  });
+  console.log(result);
+  page.locator('#main-auto').fill("Restaurants");
+  await page.keyboard.press("Enter");
+  await expect(page.locator('#main-auto')).toHaveValue("Restaurants");
+
+});
